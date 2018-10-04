917ee28c3627ac89d0807269780f977c1ee7e6d9
Diego Atance <32869877+KPsychO@users.noreply.github.com>
2018-10-04 17:43:04 +0200

    Add files via upload

---
+++ Acepta_El_Reto/problema_116.cpp
@@ -0,0 +1,20 @@
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    string str = "Hola mundo.\n";
+    int n;
+
+    cin >> n;
+
+    while(n--){
+
+        cout << str;
+
+    }
+
+    return 0;
+}
